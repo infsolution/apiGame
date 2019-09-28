5a38ed2b849e2d88ba0f49ce077c3c4af46f3ee1
infsolution <clsinfsolution@gmail.com>
2019-09-28 16:46:35 -0300

    estatic root import

--- shorten/urls.py
+++ shorten/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-
+from django.conf import settings
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
