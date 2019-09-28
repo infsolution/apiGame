8ca76b6bafae539506f3061f1d829d0f51a83764
infsolution <clsinfsolution@gmail.com>
2019-09-28 16:22:22 -0300

    add token

--- shorten/urls.py
+++ shorten/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('game/',include('game.urls')),
+    path('api-token-auth/', obtain_auth_token),
 ]
